bb9295bdde1a3e79fb72f303cb83c2a2359f278b
Harshit Jain <121855889+HarshitJain1924@users.noreply.github.com>
2024-05-02 22:03:27 +0530

    Delete public directory

--- public/js/payment.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // JavaScript code here
-    var myLink = document.querySelector('a[href="#"]');
-    myLink.addEventListener('click', function(e) {
-        e.preventDefault();
-    });
-});
